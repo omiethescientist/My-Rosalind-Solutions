48f407c3179da81e61de42e2eece266113788abd
omiethescientist <kanaomar@msu.edu>
2022-09-25 07:53:18 -0400

    Adding Corrections to Enumerating Gene Orders

--- code/Enumerating_Gene_Orders.py
+++ code/Enumerating_Gene_Orders.py
@@ -1,6 +1,5 @@
 import sys
 n = int(sys.argv[1])
-fo = "../rosalind_perm_output.txt"
 array = [[i] for i in range(1, n+1)]
 
 def recursive_loop(array, n):
@@ -13,10 +12,8 @@ def recursive_loop(array, n):
                     new_array.append(j2)
         recursive_loop(new_array, n)
     else:
-        with open(fo, "r") as file: 
-            print(len(array))
-            for x in array:
-                print(*x, sep = " ")
-            file.close()
+        print(len(array))
+        for x in array:
+            print(*x, sep = " ")
 
 recursive_loop(array, n)
