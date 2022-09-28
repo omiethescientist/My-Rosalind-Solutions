5edab5b102d7f0113254c1f62d0a1e9f1ab1f82c
omiethescientist <kanaomar@msu.edu>
2022-09-27 22:17:23 -0400

    Adding solution to splc

---
+++ code/RNA_Splicing.py
@@ -0,0 +1,44 @@
+import sys
+import re
+
+fn = sys.argv[1]
+
+
+with open(fn) as fasta:
+    seq_list = re.split(r">\w+\d+", fasta.read().replace("\n", ""))
+    seq = seq_list[1]
+    introns = seq_list[2:]
+
+
+for intron in introns:
+    seq = "".join(seq.split(intron))
+
+rna_seq = seq.replace("T", "U") 
+rna_seq = rna_seq.replace("\n", "")
+
+codons = [rna_seq[3*i:3*i+3] for i in range(len(rna_seq)//3)]
+
+codon_map = {
+    'UUU': 'F',     'CUU': 'L',     'AUU': 'I',     'GUU': 'V',
+    'UUC': 'F',     'CUC': 'L',     'AUC': 'I',     'GUC': 'V',
+    'UUA': 'L',     'CUA': 'L',     'AUA': 'I',     'GUA': 'V',
+    'UUG': 'L',     'CUG': 'L',     'AUG': 'M',     'GUG': 'V',
+    'UCU': 'S',     'CCU': 'P',     'ACU': 'T',     'GCU': 'A',
+    'UCC': 'S',     'CCC': 'P',     'ACC': 'T',     'GCC': 'A',
+    'UCA': 'S',     'CCA': 'P',     'ACA': 'T',     'GCA': 'A',
+    'UCG': 'S',     'CCG': 'P',     'ACG': 'T',     'GCG': 'A',
+    'UAU': 'Y',     'CAU': 'H',     'AAU': 'N',     'GAU': 'D',
+    'UAC': 'Y',     'CAC': 'H',     'AAC': 'N',     'GAC': 'D',
+    'UAA': 'Stop',  'CAA': 'Q',     'AAA': 'K',     'GAA': 'E',
+    'UAG': 'Stop',  'CAG': 'Q',     'AAG': 'K',     'GAG': 'E',
+    'UGU': 'C',     'CGU': 'R',     'AGU': 'S',     'GGU': 'G',
+    'UGC': 'C',     'CGC': 'R',     'AGC': 'S',     'GGC': 'G',
+    'UGA': 'Stop',  'CGA': 'R',     'AGA': 'R',     'GGA': 'G',
+    'UGG': 'W',     'CGG': 'R',     'AGG': 'R',     'GGG': 'G'
+}
+
+aa_list = [codon_map[codon] for codon in codons]
+stop_idx = aa_list.index("Stop")
+aa_list = aa_list[:stop_idx]
+aa_seq = "".join(aa_list)
+print(aa_seq)
